c08f6b66787564914b6acc79bd7bc226709ad337
regzimarx <gergimarjohnalinsangao@gmail.com>
2021-06-29 19:14:53 +0800

    Initial portfoio website design

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+var collapse = document.querySelector('.collapse');
+var toggler = document.querySelector('.navbar-toggler');
+var navlink = document.querySelector('.nav-link');
+function toggleMobileNav(){
+  collapse.classList.toggle('show');
+}
